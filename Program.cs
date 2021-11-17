54e4a9cd1ef246f45704b299b5a25eedfb3c5271
sfree <sfree@STEVANS-PC>
2021-11-17 12:03:43 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Onspring.API.SDK;
+
+namespace consoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string baseUrl = "";
+            const string apiKey = "";
+            var onspringClient = new OnspringClient(baseUrl, apiKey);
+            Console.WriteLine("Hello World");
+        }
+    }
+}
