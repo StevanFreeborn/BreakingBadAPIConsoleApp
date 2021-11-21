9a9c4a46982189e4932fb4406ba07332a36d59ea
StevanFreeborn <sfreeborn7@gmail.com>
2021-11-21 00:21:56 -0600

    Further development

---
+++ OnspringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Onspring.API.SDK;
+using Onspring.API.SDK.Helpers;
+
+namespace consoleApplication
+{
+    public class OnspringHelper
+    {
+        public OnspringClient _client;
+
+        public OnspringHelper(string baseUrl, string apiKey)
+        {
+            _client = new OnspringClient(baseUrl, apiKey);
+        }
+    }
+}
